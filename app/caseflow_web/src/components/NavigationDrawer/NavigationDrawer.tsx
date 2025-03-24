89c9a4202c2d01f7fe19e744378a0ad4156accfd
brad-aot <137842247+brad-aot@users.noreply.github.com>
2025-03-24 12:00:02 -0700

    Changed default form name

--- app/caseflow_web/src/components/NavigationDrawer/NavigationDrawer.tsx
+++ app/caseflow_web/src/components/NavigationDrawer/NavigationDrawer.tsx
@@ -220,7 +220,7 @@ export default function MiniDrawer() {
   };
   const selectForm = () => {
     dispatch(resetSelectedCase());
-    dispatch(setSelectedCaseType("loopform"));
+    dispatch(setSelectedCaseType("MinistryOfLabourPOC"));
     setOpenPopup(false);
     navigate("cases/create");
   };
